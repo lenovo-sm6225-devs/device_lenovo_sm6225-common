8267d003fa1c80cd4b3a20ab24e94d743c791dac
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-31 23:16:18 +0700

    sm6225-common: Import common sepolicy
    - Pull from Xiaomi SM8250
    
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: Utsav Balar <utsavbalar1231@gmail.com>
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Co-authored-by: HeroBuxx <herobuxx@gmail.com>
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: boedhack99 <boedhack@gmail.com>
    Change-Id: Id09d9435519ee9b9316b8103f557f269947d3b7d
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,8 @@ VENDOR_SECURITY_PATCH := 2022-04-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
