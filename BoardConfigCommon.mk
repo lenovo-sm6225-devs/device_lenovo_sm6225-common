4c72a6ad9ccbd81727655fd6b5d3ccaf9078e9e0
luk1337 <priv.luk@gmail.com>
2022-06-04 15:30:33 +0700

    sm6225-common: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,13 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_lenovo_bengal
 TARGET_RECOVERY_DEVICE_MODULES ?= init_lenovo_bengal
--- bengal.mk
+++ bengal.mk
@@ -220,11 +220,6 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
