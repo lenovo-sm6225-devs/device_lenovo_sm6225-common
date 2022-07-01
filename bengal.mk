c9226e981b41e4d1175e5661399ce303486bd890
boedhack99 <boedhack@gmail.com>
2022-07-01 17:56:31 +0700

    sm6225-common: vibrator: Move excluded-input-devices to vibrator folder section
    Change-Id: Ie2b058a3dfe75e6a22561f95b3e62f4e3932979f
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- bengal.mk
+++ bengal.mk
@@ -430,7 +430,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.lenovo_bengal
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
