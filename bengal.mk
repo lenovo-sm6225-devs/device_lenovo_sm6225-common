44cb669a30754294a1f4561951329a7c6542afff
Ramii ahmed <ramy@ahmedramy.com>
2022-07-03 17:48:10 +0700

    sm6225-common: Build vendor.qti.hardware.servicetracker@1.2 HIDL E ANDR-PERF-LM: Core: lmMain() 327: Unable to load library libprekill.so: dlopen failed: library vendor.qti.hardware.servicetracker@1.0.so not found: needed by /vendor/lib64/libprekill.so in namespace (default)
    Change-Id: I4914b217495e83e34f69b730373633e84cf5f1e7

--- bengal.mk
+++ bengal.mk
@@ -381,6 +381,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-service \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
