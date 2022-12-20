bee0bb019b01dae40d12281a20c9df17b855d27c
Hadenix <vsegda_online_007@bk.ru>
2022-12-21 00:12:46 +0300

    sm6225-common: Add kernel flags
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,5 +23,31 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_IMAGE_NAME  := Image
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_TAGS_OFFSET        := 0x00000100
+
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE += \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=4e00000.dwc3 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0x4a90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048
+
+TARGET_KERNEL_SOURCE := kernel/lenovo/tb128fu
+
 # Inherit the proprietary files
 include vendor/lenovo/sm6225-common/BoardConfigVendor.mk
