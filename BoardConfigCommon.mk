dfba10cef5e4e9937a945971f02f067c1f4d7ad0
Hadenix <vsegda_online_007@bk.ru>
2022-12-20 22:41:41 +0300

    sm6225-common: Initial skeleton tree
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/lenovo/sm6225-common
+
+# Inherit the proprietary files
+include vendor/lenovo/sm6225-common/BoardConfigVendor.mk
