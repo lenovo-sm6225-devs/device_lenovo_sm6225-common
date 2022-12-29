dblenk-project/device_xiaomi_sm6225-common

sm6225-common: Pull libinit from xiaomi-sm8250
* Ref: https://github.com/LineageOS/android_device_xiaomi_sm8250-common/commits/lineage-19.1/libinit

* sm6225-common: Cleanup and adapt to bengal

Signed-off-by: boedhack99 <boedhack@gmail.com>
Change-Id: I4b2c0f8b387106fc67e7100fbe6cdc1ab6d6d691

BoardConfigCommon.mk
@@ -79,6 +79,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibi
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_bengal
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_bengal
+
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_IMAGE_NAME  := Image

lenovo-sm6225-devs/device_lenovo_sm6225-common

Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

BoardConfigCommon.mk
@@ -57,6 +57,10 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_lenovo_bengal
+TARGET_RECOVERY_DEVICE_MODULES ?= init_lenovo_bengal
+
 # Kernel
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_IMAGE_NAME  := Image