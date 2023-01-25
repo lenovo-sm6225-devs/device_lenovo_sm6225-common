dblenk-project/device_xiaomi_sm6225-common

sm6225-common: sensors: Avoid target name conflicts
Change-Id: If7bd530f2be559f238666a842e9ba5f8e050aeb4

bengal.mk
@@ -397,7 +397,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi_bengal-multihal \
     libsensorndkbridge
 
 # Servicetracker

lenovo-sm6225-devs/device_lenovo_sm6225-common

bengal.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.lenovo_bengal-multihal \
     libsensorndkbridge
 
 # Servicetracker